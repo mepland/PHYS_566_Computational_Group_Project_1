c43a4acec5d057190e26b9263ae619eb3927a0c7
Matthew Epland <matthewepland@gmail.com>
2016-03-17 23:01:27 -0400

    part 3 basics in

--- code/dla.py
+++ code/dla.py
@@ -30,8 +30,8 @@ Dy = 1.0 # Delta y of our lattice
 Lx = 4*R_start/Dx # length in x of our lattice 
 Ly = 4*R_start/Dy # length in y of our lattice 
 
-d_kill = 50.0 # random walker kill distance
-t_kill = 100 # random walker kill time
+d_kill = 120.0 # random walker kill distance
+step_kill = 10000 # random walker kill step
 
 
 ########################################################
@@ -47,8 +47,8 @@ print 'Lattice Delta y = %.1f' % Dy
 print 'Lattice L x = %.1f' % Lx
 print 'Lattice L y = %.1f' % Ly
 
-print '\nRandom Walker Kill Distance d_kill = %.1f' % d_kill
-print 'Random Walker Kill Time t_kill = %d' % t_kill
+print '\nRandom Walker Kill Distance, d_kill = %.1f' % d_kill
+print 'Random Walker Kill Step, step_kill = %d' % step_kill
 
 
 print '\n---------------------------------------------'
@@ -85,7 +85,8 @@ class cluster_point:
 			if (-Ly/2 + Dy*(i-0.5)) <= rand_y and rand_y < (-Ly/2 + Dy*(i+0.5)):
 				self.y = -Ly/2 + Dy*i
 
-		# make sure we don't overlap an existing cluster point, repeat the whole thing if we do
+		# make sure we don't overlap an existing cluster point, repeat from the top if we do
+		# if len(cluster) == 0 this is the seed and we don't need to check...
 		overlap = False
 		j = 0
 		while j < len(cluster) and not overlap:
@@ -95,7 +96,7 @@ class cluster_point:
     # end def for constructor
 
     # Execute one random walk step
-    def step(self):
+    def random_walk_step(self):
 	# make sure we don't try to move a fixed cluster point
 	if self.fixed:
 		print 'ERROR!! Attempted to walk a fixed cluster point, exiting!!'
@@ -110,35 +111,68 @@ class cluster_point:
 
 	return None # return None explicitly, python will automatically but this is clearer to the reader
     
-    # end def for step()
+    # end def for random_walk_step()
 
-    # Return spatial separation r from the nearest cluster point
-    def r_min(self, cluster = []):
-
-	# Find the nearest point
-	r_min = 10*R_start
+    # Return spatial separation d from the nearest cluster point
+    def d_min(self, cluster = []):
+	rMin = 2*max(Lx, Ly)
 	for i in range(len(cluster)):
 		r = math.sqrt( (self.x - cluster[i].x)**2 + (self.y - cluster[i].y)**2 )
-		if r < r_min: r_min = r
+		if r < rMin: rMin = r
+	return rMin
+    # end def for d_min()
 
-	return r_min
-    # end def for r_min()
+    # See if adjacent to an existing cluster point
+    def touching(self, cluster = []):
+	touch = False
+	for i in range(len(cluster)):
+		if cluster[i].x == self.x + Dx and cluster[i].y == self.y: touch = True # right
+		elif cluster[i].x == self.x and cluster[i].y == self.y + Dy: touch = True # top
+		elif cluster[i].x == self.x - Dx and cluster[i].y == self.y: touch = True # left
+		elif cluster[i].x == self.x and cluster[i].y == self.y - Dy: touch = True # bottom
+
+	return touch
+    # end def for touching()
 
 # end class for cluster_point
-
 
 # Define a function to get a new cluster point
 def new_cluster_point(index, cluster = []):
 	if(debugging): print 'Beginning new_cluster_point, index = %d ' % index	
 
-	# repeat until it hits the cluster
-	m_walker = cluster_point(index, cluster)
+	# repeatly generate m_walker until one hits the cluster
 
-	# TODO walk the cluster until one of the three halt conditions are met
-
-
-	# return the cluster_point
-
+	status = 0
+	while status != 1:
+	
+		status = 0
+		m_walker = cluster_point(index, cluster)
+	
+		# walking loop until one of the three halt conditions is met, ie status = 1, 2, or 3
+		while status == 0:
+		
+			# Check to see if the walker hit a cluster point, and can join the cluster
+			if(m_walker.touching(cluster)):
+				status = 1
+				m_walker.fixed = True
+				if(debugging2): print 'Hit the Cluster!!'
+				continue
+	
+			# Check to see if the walker is too far away 
+			if(m_walker.d_min(cluster) > d_kill):
+				status = 2
+				if(debugging2): print 'Walked too far, d_min = %.2f > d_kill = %.2f on step_number = %d ' % (m_walker.d_min(cluster), d_kill, m_walker.step_number)
+	
+			# Check to see if the walker has run too long 
+			if(m_walker.step_number > step_kill):
+				status = 3
+				if(debugging2): print 'Walked too long, d_min = %f ' % m_walker.d_min(cluster)
+	
+			# Perform another random walk step 
+			m_walker.random_walk_step()
+	
+	# return the cluster point
+	return m_walker
 # end def for new_cluster_point()
 
 
@@ -236,12 +270,21 @@ def plot(optional_title, m_path, fname):
 ########################################################
 # Development Runs 
 
-if(False):
-	output_path = './output/dev'
+if(True):
+	output_path = '../output/problem_3/dev'
 	debugging = True
+	debugging2 = True
+
+	# Set up the cluster with the seed at (0,0)
+	cluster = []
+	cluster.append(cluster_point(-1, cluster))
+	cluster[0].x = 0.0
+	cluster[0].y = 0.0
+	cluster[0].fixed = True
 
 
-
+	for i in range(5):
+		cluster.append(new_cluster_point(i, cluster))
 
 
 ########################################################
@@ -249,8 +292,9 @@ if(False):
 # Production Runs for paper 
 
 if(False):
-	top_output_path = './output/plots_for_paper'
+	top_output_path = '../output/plots_for_paper/problem_3'
 	debugging = False
+	debugging2 = False
 
         # Part a
         ########################################################
@@ -265,6 +309,7 @@ if(False):
         output_path = top_output_path+'/part_b'
 
 	# TODO
+
 
 
 ########################################################
